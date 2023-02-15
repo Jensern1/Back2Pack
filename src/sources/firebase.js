2d92a37b6317cc24d3c3da7ef49217a6ee3dfc79
Jens <jensesk@stud.ntnu.no>
2023-02-15 09:08:30 +0100

    Firestore tutorial

--- src/sources/firebase.js
+++ src/sources/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBPHhZjnX7r2RXODrTjB47cNh2RIGIJnbg',
-  authDomain: 'pu-backpakking.firebaseapp.com',
-  projectId: 'pu-backpakking',
-  storageBucket: 'pu-backpakking.appspot.com',
-  messagingSenderId: '634044469514',
-  appId: '1:634044469514:web:fdeca9d99765d2d4f8eaf3',
-  measurementId: 'G-YG9HM34W3G',
+  apiKey: "AIzaSyBPHhZjnX7r2RXODrTjB47cNh2RIGIJnbg",
+  authDomain: "pu-backpakking.firebaseapp.com",
+  projectId: "pu-backpakking",
+  storageBucket: "pu-backpakking.appspot.com",
+  messagingSenderId: "634044469514",
+  appId: "1:634044469514:web:fdeca9d99765d2d4f8eaf3",
+  measurementId: "G-YG9HM34W3G",
 };
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
+const db = getFireStore(app);
 
-export default firebase;
+//export default firebase;
+export default db;
