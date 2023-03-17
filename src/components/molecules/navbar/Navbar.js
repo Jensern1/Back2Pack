3d0d522ff526459aa9bdc8f57b77f5a6ede18141
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2023-03-17 10:05:14 +0100

    made loggout

--- src/components/molecules/navbar/Navbar.js
+++ src/components/molecules/navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { useRef } from "react";
-import { useMediaQuery } from "@chakra-ui/react";
+import React, { useContext } from 'react';
+import { useRef } from 'react';
+import { useMediaQuery } from '@chakra-ui/react';
 import {
   Menu,
   MenuButton,
@@ -10,38 +10,40 @@ import {
   MenuGroup,
   MenuOptionGroup,
   MenuDivider,
-} from "@chakra-ui/react";
-import { Box, Flex, Button, Text, Input, IconButton } from "@chakra-ui/react";
+} from '@chakra-ui/react';
+import { Box, Flex, Button, Text, Input, IconButton } from '@chakra-ui/react';
 import {
   AiOutlineHome,
   AiOutlineUser,
   AiOutlineSearch,
   AiOutlinePlus,
-} from "react-icons/ai";
-import { HamburgerIcon, AddIcon } from "@chakra-ui/icons";
-import styles from "./Navbar.module.scss";
-import AddBtn from "../../atoms/addBtn/AddBtn";
+} from 'react-icons/ai';
+import { HamburgerIcon, AddIcon } from '@chakra-ui/icons';
+import styles from './Navbar.module.scss';
+import AddBtn from '../../atoms/addBtn/AddBtn';
+import { UserContext } from 'contexts/UserContext.js';
 
 const Navbar = ({ onAddTrip, searchInput, handleSearch }) => {
-  const [phone] = useMediaQuery("(max-width: 800px)");
+  const [phone] = useMediaQuery('(max-width: 800px)');
+  const { loggOut } = useContext(UserContext);
 
   return (
     <Box className={styles.navbar}>
       {phone ? (
-        <Flex alignItems="center" justifyContent={"space-between"} flex="1">
-          <Text fontSize="4xl" fontFamily="cursive">
+        <Flex alignItems='center' justifyContent={'space-between'} flex='1'>
+          <Text fontSize='4xl' fontFamily='cursive'>
             Back2Pack
           </Text>
           <Menu>
             <MenuButton
               as={IconButton}
-              aria-label="Options"
+              aria-label='Options'
               icon={<HamburgerIcon />}
-              variant="outline"
-              size="lg"
+              variant='outline'
+              size='lg'
             />
             <MenuList>
-              <MenuItem icon={<AddIcon />} command="⌘T">
+              <MenuItem icon={<AddIcon />} command='⌘T'>
                 New Tab
               </MenuItem>
             </MenuList>
@@ -49,54 +51,57 @@ const Navbar = ({ onAddTrip, searchInput, handleSearch }) => {
         </Flex>
       ) : (
         <>
-          <Flex alignItems="center" flex="1">
-            <Box mr="5">
-              <Text fontSize="4xl" fontFamily="cursive">
+          <Flex alignItems='center' flex='1'>
+            <Box mr='5'>
+              <Text fontSize='4xl' fontFamily='cursive'>
                 Back2Pack
               </Text>
             </Box>
-            <a href="http://localhost:3000">
-              <Button variant="ghost" leftIcon={<AiOutlineHome />} size="lg">
+            <a href='http://localhost:3000'>
+              <Button variant='ghost' leftIcon={<AiOutlineHome />} size='lg'>
                 Home
               </Button>
             </a>
-            <a href="http://localhost:3000/User">
-              <Button variant="ghost" leftIcon={<AiOutlineUser />} size="lg">
+            <a href='http://localhost:3000/User'>
+              <Button variant='ghost' leftIcon={<AiOutlineUser />} size='lg'>
                 User
               </Button>
             </a>
           </Flex>
-          <Flex alignItems="center" flex="3" justifyContent="center">
+          <Flex alignItems='center' flex='3' justifyContent='center'>
             <Input
-              placeholder="Search"
-              size="md"
+              placeholder='Search'
+              size='md'
               value={searchInput}
               onChange={(event) => handleSearch(event.target.value)}
-              w="50%"
+              w='50%'
             />
-            <Box ml="5">
+            <Box ml='5'>
               <AddBtn
-                colorScheme="teal"
-                aria-label="addBtn"
-                size="lg"
+                colorScheme='teal'
+                aria-label='addBtn'
+                size='lg'
                 icon={<AiOutlinePlus />}
                 onClick={() => onAddTrip(true)}
               />
             </Box>
           </Flex>
           <Flex
-            marginLeft="auto"
-            marginRight="0"
-            justifyContent="flex-end"
-            alignItems="center"
-            flex="1"
+            marginLeft='auto'
+            marginRight='0'
+            justifyContent='flex-end'
+            alignItems='center'
+            flex='1'
           >
-            <Box bg="#ffffff" borderRadius="full" p={2} mr={2}>
-              <AiOutlineUser size={32} color="#1e212d" />
+            <Box bg='#ffffff' borderRadius='full' p={2} mr={2}>
+              <AiOutlineUser size={32} color='#1e212d' />
             </Box>
-            <Text fontSize="xl" fontWeight="bold">
+            <Text fontSize='xl' fontWeight='bold'>
               Username
             </Text>
+            <Button ml={10} variant='outline' onClick={() => loggOut()}>
+              Logout
+            </Button>
           </Flex>
         </>
       )}
