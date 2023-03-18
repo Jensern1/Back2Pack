07c3612527cfe31d1a46c337f9e8783268a5f655
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2023-03-18 15:41:03 +0100

    fex: fix saving to lochal storrage v2

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,48 +1,50 @@
-import React, { createContext, useEffect } from "react";
-import { useState } from "react";
+import React, { createContext } from 'react';
+import { useState } from 'react';
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [name, setName] = useState(window.localStorage.getItem("name"));
-  const [email, setEmail] = useState(window.localStorage.getItem("email"));
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [userID, setUserID] = useState(null);
+  const [name, setName] = useState(window.localStorage.getItem('name'));
+  const [email, setEmail] = useState(window.localStorage.getItem('email'));
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
+  const [userID, setUserID] = useState(window.localStorage.setItem('uid'));
   const [isLoggedIn, setLoggedIn] = useState(
-    window.localStorage.getItem("isLoggedIn") === "true"
+    window.localStorage.getItem('isLoggedIn') === 'true'
   );
 
-  function setUser(uid, fname, femail, ftoken) {
-    if (fname === null || fname === undefined || fname === "") {
-      let splitname = femail.split("@")[0];
-      setName(splitname.split(".")[0]);
-    } else {
-      setName(fname);
+  function setUser(fuid, fname, femail, ftoken) {
+    let newName = fname;
+    if (fname === null || fname === undefined || fname === '') {
+      let splitname = femail.split('@')[0];
+      newName = splitname.split('.')[0];
     }
-    setUserID(uid);
-    console.log("UID = " + uid);
+
+    setName(newName);
+    setUserID(fuid);
     setEmail(femail);
     setToken(ftoken);
-
     setLoggedIn(true);
-    console.log("User set!");
 
-    window.localStorage.setItem("name", name);
-    window.localStorage.setItem("email", email);
-    window.localStorage.setItem("token", token);
-    window.localStorage.setItem("isLoggedIn", "true");
+    console.log('User set!');
+
+    window.localStorage.setItem('name', newName);
+    window.localStorage.setItem('email', femail);
+    window.localStorage.setItem('token', ftoken);
+    window.localStorage.setItem('uid', fuid);
+    window.localStorage.setItem('isLoggedIn', 'true');
   }
 
   function loggOut() {
-    setName("");
-    setEmail("");
-    setToken("");
+    setName('');
+    setEmail('');
+    setToken('');
     setLoggedIn(false);
-    console.log("User logged out!");
+    console.log('User logged out!');
 
-    window.localStorage.setItem("name", "");
-    window.localStorage.setItem("email", "");
-    window.localStorage.setItem("token", "");
-    window.localStorage.setItem("isLoggedIn", "false");
+    window.localStorage.setItem('name', '');
+    window.localStorage.setItem('email', '');
+    window.localStorage.setItem('token', '');
+    window.localStorage.setItem('uid', '');
+    window.localStorage.setItem('isLoggedIn', 'false');
   }
 
   return (
