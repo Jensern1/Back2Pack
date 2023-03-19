8ce14c59945bfe4253dbd180556d449420d63ec2
sofiefe <sofiefe@stud.ntnu.no>
2023-03-19 13:49:45 +0100

    added ProfileDisplay to User page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from '../../components/molecules/navbar/Navbar.js';
+import { UserContext } from "../../../contexts/UserContext.js";
+import ProfileDisplay from './components/molecules/profile/profile.js';
+//Importert profilbilde, bør kunne hentes
+import profilePicture from "./components/molecules/profile/strand.jpg"
+
+const { name, email } = useContext(UserContext);
 
 function User() {
   return (
     <>
       <Navbar></Navbar>
       <p>Hello User!</p>
+      <ProfileDisplay
+        name={name}
+        email={email}
+        profilePicture={profilePicture}
+      />
     </>
   );
 }
